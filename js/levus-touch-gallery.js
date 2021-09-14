90577fc24edd24e71c2d03e48d3196a5aa9bfa0b
lvivduncan <lvivduncan@gmail.com>
2021-09-14 14:46:46 +0300

    dots

--- js/levus-touch-gallery.js
+++ js/levus-touch-gallery.js
@@ -1,7 +1,11 @@
 // TODO: клікабельні тумбіки на десктопі (скрол), на мобільному крапки. велике фото має лайтбокс
 // TODO: add all gallery
+// TODO: fix thumbs animation
+// TODO: thumbs crop image
+// TODO: replace forEach with for-of
 
 {
+    const body = document.getElementsByTagName('body')[0];
     const gallery = document.querySelector('.levus-touch-gallery');
 
     // insert icon 'view'
@@ -21,11 +25,16 @@
 
     if(slides.length <= 7){
 
-        // TODO: add for 7 item
-        slides.forEach(slide => {
-            const clone = slide.cloneNode(true);
+        // slides.forEach(slide => {
+        //     const clone = slide.cloneNode(true);
+        //     slider.append(clone);
+        // });
+
+        for(let i of slides){
+
+            const clone = i.cloneNode(true);
             slider.append(clone);
-        });
+        }
     }
 
     slides = gallery.querySelectorAll('.slide');
@@ -33,6 +42,14 @@
     const images = gallery.querySelectorAll('img');
 
     const length = images.length;
+
+    // мініатюри
+    const miniImages = [];
+
+    for(let i of images){
+
+        miniImages.push(i.dataset.thumb);
+    }
 
     // check dragable
     let drag = false;
@@ -69,6 +86,16 @@
     // const origin = [...elements];
     const origin = elements.slice(0);
 
+    // create lightbox div
+    const lightbox = document.createElement('div');
+    lightbox.id = 'levus-lightbox';
+
+    // temp container for lightbox elements 
+    let insertData = '';
+
+    // opacity current element
+    let opacity = 0;
+
     // set maximum height of the slider
     setMaxHeightSlider();
 
@@ -82,7 +109,7 @@
 
         // disable drag image
         slide.addEventListener('dragstart', event => event.preventDefault());
-        slide.setAttribute('draggable',false);
+        // slide.setAttribute('draggable',false);
 
         slide.addEventListener('pointerdown', scrollStart);
         slide.addEventListener('pointermove', scrollMove);
@@ -90,6 +117,7 @@
         slide.addEventListener('pointercancel', scrollEnd);
         slide.addEventListener('pointerleave', scrollEnd);
 
+        // disable click
         slide.addEventListener('click', clickSlide);
     });
 
@@ -98,6 +126,89 @@
         image.addEventListener('click', () => {
             clickThumb(index);
         });
+    });
+
+    // TODO: click icon -- open slide[id]
+    gallery.addEventListener('click', event => {
+
+        // click to icon -- for open lightbox
+        if(event.target.classList.contains('icon')){
+
+            slides = gallery.querySelectorAll('.slide');
+
+            for(let i = 0; i < length; i++){
+
+                // active slide
+                if(slides[i].style.opacity == 1){
+                    
+                    // <picture style="transform:translateX(${elements[i]}%);opacity:${opacity}">
+                    insertData += `
+                        <picture style="transform:translateX(${elements[i]}%);opacity:1">
+                            <img 
+                                src="${slides[i].href}" 
+                                alt="${slides[i].title}" 
+                                draggable="false" 
+                                class="levus-lightbox-picture">
+                        </picture>`;  
+
+                    // prepared data to insert to body
+                    lightbox.innerHTML = insertData;
+
+                    // isert data to body
+                    body.append(lightbox);
+
+                    setTimeout(() => {
+                        
+                        lightbox.className = 'active';
+                    }, 60);
+                }
+
+            }
+        }
+
+        // click dot -- for scroll
+        if(event.target.tagName === 'LI'){
+
+            const dots = gallery.querySelectorAll('.dots li');
+
+            const id = event.target.dataset.id;
+            
+            for(let i = 0; i < length; i++){
+
+                dots[i].classList.remove('active');
+                event.target.classList.add('active');
+            }
+
+            elements.length = 0;
+
+            // clone
+            elements = origin.slice(0);
+    
+            // assign
+            const crop = elements.splice(0,length - id);
+            elements.push(...crop);
+    
+            render();
+        }
+    });
+
+    // close lightbox
+    lightbox.addEventListener('click', event => {
+
+        if(event.target.tagName === 'PICTURE'){
+
+            closeLightbox();
+        }
+    });
+
+    // close lightbox
+    document.addEventListener('keydown', event => {
+
+        // close lightbox
+        if(event.key === 'Escape' || event.code === 'Escape'){
+
+            closeLightbox();
+        }
     });
 
     function setMaxHeightSlider(){
@@ -138,8 +249,6 @@
 
             // where it was moved
             finish = event.pageX;
-
-            console.log(start, finish)
 
             // if to left
             if(finish - start < 0){
@@ -211,6 +320,11 @@
         // thumbs images
         const thumbs = gallery.querySelectorAll('.thumbs img');
 
+        // block for dots
+        const dots = gallery.querySelector('.dots');
+        let dotsLi = '';
+        let dotsElements = '';
+
         slides.forEach((slide, index) => {
 
             // set opacity slide img
@@ -234,7 +348,23 @@
 
             // render thumbs images
             thumb.style.transform = `translateX(${elements[index]}%)`;
+
+            // render dots li
+            if(elements[index] === 0){
+
+                // dots block
+                dotsLi = `<li data-id="${index}" class="active"></li>`;
+
+            } else {
+                
+                // dots block
+                dotsLi = `<li data-id="${index}"></li>`;
+            }
+
+            dotsElements += dotsLi;
         });
+
+        dots.innerHTML = dotsElements;
     }
 
     // move item after click
@@ -259,17 +389,29 @@
         event.preventDefault();   
     }
 
-    // TODO: click icon -- open slide[id]
-    // TODO: animation icon (hide before scroll)
+    // function clickDot(){
 
-    gallery.addEventListener('click', event => {
 
-        if(event.target.classList.contains('icon')){
+    // }
 
-            console.log('icon click')
-        }
-    })
+    // close lightbox
+    function closeLightbox(){
+
+        setTimeout(() => {
+    
+            lightbox.classList.remove('active');
+        }, 60);
+    
+        setTimeout(() => {
+            
+            lightbox.remove();
+        }, 480); 
+
+        // clear lightbox content
+        insertData = '';
+
+        // set default data
+        // setTransition();
+    }
 
 }
-
-// TODO: to left, to right drag
